9de2a13f7bf2594f5431995119865c5dff8e5bc5
fieryWaters <34853790+fieryWaters@users.noreply.github.com>
2023-08-24 01:07:50 -0500

    Bugfix: Large files now split properly

--- whisper-improved.py
+++ whisper-improved.py
@@ -22,12 +22,14 @@ if not os.path.exists(transcriptions_directory):
 # Look for all audio files in the directory the script is run from
 for filename in os.listdir(current_directory):
     if filename.endswith(('.wav', '.mp3', '.flac', '.aac', '.ogg', '.m4a')):
+        print("Importing " + str(filename))
         audio_path = os.path.join(current_directory, filename)
 
         # Load the audio file
         audio = AudioSegment.from_file(audio_path)
 
         # Convert the file to .mp3
+        print("Converting current file to .mp3")
         mp3_path = audio_path.rsplit(".", 1)[0] + ".mp3"
         audio.export(mp3_path, format="mp3")
 
@@ -36,12 +38,21 @@ for filename in os.listdir(current_directory):
         num_chunks = math.ceil(audio_file_size / (25 * 1024 * 1024))  # 25MB chunks
 
         # Split audio file into smaller chunks if it exceeds 25MB
+        print("splitting into smaller chunks now")
         chunk_length = int(len(audio) / num_chunks)
-        chunks = [audio[i:i + chunk_length] for i in range(0, len(audio), chunk_length)]
+        # chunks = [audio[i:i + chunk_length] for i in range(0, len(audio), chunk_length)]
+
+        chunk_starts = [i*chunk_length for i in range(num_chunks)]
+        chunk_ends = chunk_starts[1:] + [len(audio)]  # Use the length of the audio as the end for the last chunk
+
+        chunks = [audio[start:end] for start, end in zip(chunk_starts, chunk_ends)]
+
 
         # Transcribe each chunk
+        print("transcribe each chunk")
         transcripts = []
         for i, chunk in enumerate(chunks):
+            print("Transcribing chunk "+ str(i+1) + "/" + str(len(chunks)))
             chunk.export("chunk.mp3", format="mp3")
 
             with open("chunk.mp3", "rb") as chunk_file:
@@ -59,3 +70,7 @@ for filename in os.listdir(current_directory):
         # Remove temporary .mp3 files
         os.remove(mp3_path)
         os.remove("chunk.mp3")
+        print()
+
+print("All Files Processed")
+input("press enter to continue")
