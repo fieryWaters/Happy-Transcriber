a786798f852488c202dbc9f9ad52d17a1f6f0dbe
fierywaters13 <fierywaters13@gmail.com>
2024-03-12 23:29:21 -0700

    Update Requirement, added scripts to make it easier

---
+++ install_requirement.py
@@ -0,0 +1,4 @@
+from utils import install_requirements
+
+if __name__ == "__main__":
+    install_requirements()
---
+++ save_requirement.py
@@ -0,0 +1,4 @@
+from utils import save_requirements
+
+if __name__ == "__main__":
+    save_requirements()
